98c0dd7b81c696cc5d3313616f0a42c0db36bfdc
keisuke ohashi <kog71903@gmail.com>
2020-01-19 15:54:36 +0900

    [migrate]devise_token_authの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: "home#index"
-
+  root to: "home#index"ga 
+  
   namespace 'api' do
     namespace 'v1' do
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
 end
