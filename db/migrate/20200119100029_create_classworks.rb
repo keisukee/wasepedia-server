3ab4e0d2b277a5396de5128f97c591a47f800e32
keisuke ohashi <kog71903@gmail.com>
2020-01-19 19:00:56 +0900

    [migrate]Model Classworkを作成

---
+++ db/migrate/20200119100029_create_classworks.rb
@@ -0,0 +1,11 @@
+class CreateClassworks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :classworks do |t|
+      t.string :name
+      t.float :difficulty
+      t.float :quality
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_095458) do
+ActiveRecord::Schema.define(version: 2020_01_19_100029) do
+
+  create_table "classworks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.float "difficulty"
+    t.float "quality"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "universities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
