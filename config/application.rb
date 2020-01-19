fb7dac75fd3c29fd601992ed0579c29966296391
keisuke ohashi <kog71903@gmail.com>
2020-01-19 15:34:16 +0900

    [migrate]User作成

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module AppName
       g.helper false
       g.template_engine :slim
       g.test_framework :rspec, view_specs: false, helper_specs: false, fixture: true
-      g.fixture_replacement :factory_girl, dir: "spec/support/factories"
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
